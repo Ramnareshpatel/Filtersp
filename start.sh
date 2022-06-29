f28fd38bc14234530339e5706f6d800d3a6c5910
TG-SURAJ <108281894+TG-SURAJ@users.noreply.github.com>
2022-06-29 18:38:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Ramnareshpatel/mainrepo.git /LUNA-EXTRA-FEATURES
+  git clone https://github.com/TG-SURAJ/Melody-Filter-Bot.git /Melody-Filter-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LUNA-EXTRA-FEATURES
+  git clone $UPSTREAM_REPO /Melody-Filter-Bot
 fi
-cd /LUNA-EXTRA-FEATURES
+cd /Melody-Filter-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Mᴇʟᴏᴅʏ 💖..."
 python3 bot.py
