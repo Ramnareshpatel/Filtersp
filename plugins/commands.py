408ca51f5e457675570b9626696c41d1f0e1539c
Sᴜʀᴀᴊ <108281894+TG-SURAJ@users.noreply.github.com>
2022-09-14 09:00:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -38,7 +38,7 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
-        T = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
+        T = datetime.datetime.now(pytz.timezone("Kolkata"))
 
         Time = T.hour
         
@@ -91,7 +91,7 @@ async def start(client, message):
             )
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
-        T = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
+        T = datetime.datetime.now(pytz.timezone("Kolkata"))
 
         Time = T.hour
         
